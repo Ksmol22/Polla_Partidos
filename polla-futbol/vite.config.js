bf4bbe5b203ee3e786075a5f4c772098b7d38948
Kevin Molina <kevin.molina@laascension.com>
2025-09-04 15:45:46 -0500

    Update homepage URL and base path for Vite configuration

--- polla-futbol/vite.config.js
+++ polla-futbol/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/polla-futbol/', // Nombre de tu repositorio
+  base: '/Polla_Partidos/', // Nombre de tu repositorio
   build: {
     outDir: 'dist'
   }
